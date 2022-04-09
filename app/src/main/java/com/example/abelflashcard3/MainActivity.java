840e4b89d1d8970c0627693a4806d6bfc4a8cace
abeloy4 <93501728+abeloy4@users.noreply.github.com>
2022-04-08 22:13:13 -0400

    add logo

--- app/src/main/java/com/example/abelflashcard3/MainActivity.java
+++ app/src/main/java/com/example/abelflashcard3/MainActivity.java
@@ -3,20 +3,26 @@ package com.example.abelflashcard3;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.security.AccessController;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     FlashcardDatabase flashcardDatabase;
     List<Flashcard> allFlashcards;
+
     int currentCardDisplayedIndex = 0;
 
     @Override
@@ -28,28 +34,39 @@ public class MainActivity extends AppCompatActivity {
         TextView question = findViewById(R.id.flashcard_question);
         TextView answer = findViewById(R.id.flashcard_answer);
 
-
-
         flashcardDatabase = new FlashcardDatabase(getApplicationContext());
         allFlashcards = flashcardDatabase.getAllCards();
 
         if (allFlashcards != null && allFlashcards.size() > 0) {
-            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
-            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+            question.setText(allFlashcards.get(0).getQuestion());
+            answer.setText(allFlashcards.get(0).getAnswer());
         }
 
-        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+        question.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
-                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                int cx = answer.getWidth() / 2;
+                int cy = answer.getHeight() / 2;
+                float finalRadius = (float) Math.hypot(cx, cy);
+
+                Animator animo = ViewAnimationUtils.createCircularReveal(answer, cx, cy , 2, finalRadius);
+
+                question.setVisibility(View.INVISIBLE);
+                answer.setVisibility(View.VISIBLE);
+
+                animo.setDuration(3000);
+                animo.start();
+
             }
         });
         addIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
                 MainActivity.this.startActivityForResult(intent, 100);
+                overridePendingTransition(R.anim.right_in, R.anim.left_out);
+
             }
 
         });
@@ -62,17 +79,33 @@ public class MainActivity extends AppCompatActivity {
                 if (currentCardDisplayedIndex >= allFlashcards.size() - 1){
                     currentCardDisplayedIndex = 0;
                 }
+                final Animation leftOutAnim = AnimationUtils.loadAnimation(view.getContext(), R.anim.left_out);
+                final Animation rightInAnim = AnimationUtils.loadAnimation(view.getContext(), R.anim.right_in);
 
-                allFlashcards = flashcardDatabase.getAllCards();
-                Flashcard flashcard = allFlashcards.get(currentCardDisplayedIndex);
+                question.startAnimation(leftOutAnim);
 
-                question.setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
-                question.setVisibility(View.VISIBLE);
-                answer.setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
-                answer.setVisibility(View.INVISIBLE);
+                leftOutAnim.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        question.startAnimation(leftOutAnim);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        question.startAnimation(rightInAnim);
+                        question.setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                        question.setVisibility(View.VISIBLE);
+                        answer.setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+                        answer.setVisibility(View.INVISIBLE);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
             }
         });
-
 
     }
     @Override
